c5e986d581b1ac5394a637921a3077b31d4a9125
Uni <tenbitsrwlm@gmail.com>
2026-01-30 23:28:00 +0300

    Ex4: Добавлен тест на GET запрос "get_text"

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
 
     @Test
-    public void testMyName(){
-        System.out.println("Hello from Vlad");
+    public void testGetHello(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    public void testGetText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+        String body = response.getBody().asString();
+        assertEquals("<html>\n  <body>Hello, world</body>\n</html>", body);
     }
 }
