b58d72eef4774c98ed6078aa126903b940d09807
Uni <tenbitsrwlm@gmail.com>
2026-01-30 22:46:49 +0300

    Ex3: Создание репозитория

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello world");
+    public void testMyName(){
+        System.out.println("Hello from Vlad");
     }
 }
